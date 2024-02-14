6b5f23f958817023549b49f61f3603a038812e03
EthanWilson204 <ehwilson.business@gmail.com>
2024-02-14 10:18:51 -0500

    added Copy constructor in Car.cpp and defined it in Car.h

---
+++ Car.h
@@ -0,0 +1,31 @@
+#ifndef			__H__CAR__
+#define			__H__CAR__
+
+#include "ServiceRecord.h"
+#include <string>
+#include <vector>
+
+class Car {
+	public:
+		Car(int, std::string);
+		Car(const Car& copy);
+		int getId() const;
+		void setYear(int);
+		int getYear() const;
+		void setModel(std::string);
+		std::string getModel() const;
+		std::vector<ServiceRecord> getRecords() const;
+		void addRecord(ServiceRecord);
+
+		friend std::ostream& operator<<(std::ostream& of, const Car& car);
+	private:
+		static int current_id;
+		int id;
+		int year;
+		std::string model;
+		std::vector<ServiceRecord> records;
+
+};
+
+
+#endif
